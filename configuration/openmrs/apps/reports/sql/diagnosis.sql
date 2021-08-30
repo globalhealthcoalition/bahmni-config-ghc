813c0fcb751f259789725ec2e59bd5580a45099b
MAKOBA REAGAN PATRICK <47450907+reagan-meant@users.noreply.github.com>
2021-08-30 20:18:54 +0300

    GHC-87: Fix the diagnoses report (#16)

--- configuration/openmrs/apps/reports/sql/diagnosis.sql
+++ configuration/openmrs/apps/reports/sql/diagnosis.sql
@@ -1,12 +1,10 @@
 select  patient_identifier.identifier as 'Patient Identifier',
-  YEAR(now())-year(patient.birthdate) as Age,
-  patient.gender as Gender,
-  confirmed_diagnosis_view.name,
-  confirmed_diagnosis_view.obs_datetime as 'Diagnosis Date',
-  concat(provider.given_name, " ", provider.family_name) As Username,
-  confirmed_diagnosis_view.diagnosis_type As 'Coded/Non-coded'
-from confirmed_diagnosis_view
-  inner join patient_identifier on confirmed_diagnosis_view.person_id = patient_identifier.patient_id
-  inner join person patient on confirmed_diagnosis_view.person_id = patient.person_id
-  inner join person_name provider on confirmed_diagnosis_view.provider = provider.person_id
-and confirmed_diagnosis_view.obs_datetime BETWEEN '#startDate#' and '#endDate#';
+  YEAR(now())-year(person.birthdate) as Age,
+  person.gender as Gender,
+  diagnosis_concept_view.concept_short_name as Diagnosis,
+  obs.obs_datetime as 'Diagnosis Date'
+from obs
+  inner join patient_identifier on obs.person_id = patient_identifier.patient_id
+  inner join person  on obs.person_id = person.person_id
+  inner join diagnosis_concept_view  on obs.value_coded = diagnosis_concept_view.concept_id
+and obs.obs_datetime BETWEEN '#startDate#' and '#endDate#';
